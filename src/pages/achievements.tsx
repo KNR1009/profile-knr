4df8a24dd86c6417dde3b166e0641c37a51d63ef
伊東和成 <itokazunari5375@icloud.com>
2023-05-08 21:35:05 +0900

    feat: プロフィールの追加

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -16,14 +16,16 @@ export const Achievements: React.FC = () => {
       <Title title='受賞歴' />
       <Content>
         <section>
-          <h2>Qiita</h2>
+          <h2>
+            <a href='https://qiita.com/KNR109' target='_blank' rel='noopener noreferrer'>
+              Qiita
+              <span className='icon'>
+                <FaExternalLinkAlt size={15} />
+              </span>
+            </a>
+          </h2>
           <ul>
-            <li>
-              <a className='a-link' href='https://qiita.com/' target='_blank' rel='noopener noreferrer'>
-                「Qiita」
-              </a>
-              はユーザー数70万人の日本最大級のエンジニアコミュニティ
-            </li>
+            <li>Qiitaはユーザー数70万人の日本最大級のエンジニアコミュニティ</li>
             <li>2022年Top Contributorに選出 (上位0.0018%)</li>
             <li>9ヶ月連続月間いいね数ランキング1位</li>
             <li>Contributions数は3万</li>
@@ -33,20 +35,34 @@ export const Achievements: React.FC = () => {
           <Image src='/qiita-award.png' alt='Qiitaのトロフィー' width={350} height={250} />
         </section>
         <section>
-          <h2>Zenn</h2>
-          <p>経歴:&nbsp;&nbsp;新卒3年目(2023年度)</p>
-          <p>職場:&nbsp;&nbsp;40名規模の渋谷のWeb系開発会社</p>
-          <p>役職:&nbsp;&nbsp;執行役員(最年少)</p>
-          <p>職種:&nbsp;&nbsp;エンジニア×マーケター×広報</p>
+          <h2>
+            <a href='https://zenn.dev/sutamac' target='_blank' rel='noopener noreferrer'>
+              Zenn
+              <span className='icon'>
+                <FaExternalLinkAlt size={15} />
+              </span>
+            </a>
+          </h2>
+          <ul>
+            <li>4記事連続トンレド1位獲得</li>
+            <li>エンジニアマネジメントや設計方法などの記事を執筆</li>
+          </ul>
         </section>
-
-        <h2>その他</h2>
-        <p>
-          同じ職種の方や経営者や役員の方で興味を持っていただけたらフォームからどうぞ
-          <a className='link' href={process.env.NEXT_PUBLIC_CONTACT_LINK} target='_blank' rel='noopener noreferrer'>
-            <FaExternalLinkAlt size={16} />
-          </a>
-        </p>
+        <section>
+          <h2>会社関連</h2>
+          <ul>
+            <li>2023年に最年少執行役員 (新卒3年目)</li>
+          </ul>
+        </section>
+        <section>
+          <h2>その他</h2>
+          <p>
+            同じ職種の方や経営者や役員の方で興味を持っていただけたらフォームからどうぞ
+            <a className='link' href={process.env.NEXT_PUBLIC_CONTACT_LINK} target='_blank' rel='noopener noreferrer'>
+              <FaExternalLinkAlt size={16} />
+            </a>
+          </p>
+        </section>
       </Content>
       <div className='can-container container' data-aos='fade-up'>
         <div className='title'>
@@ -85,6 +101,9 @@ const Wrapper = styled.div`
       font-size: 32px;
       font-weight: bold;
     }
+  }
+  .icon {
+    margin-left: 8px;
   }
   .a-link {
     color: #1a73e8;
